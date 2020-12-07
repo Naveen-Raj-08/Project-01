ba5e563ef0cc65ca28c69874c1294fb1ed57211a
Naveen <rajnaveen677@gmail.com>
2020-12-07 22:18:22 +0530

    commit4

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#owl-demo").owlCarousel({
+    loop: true,
+    margin: 10,
+    items: 1,
+    autoplay: true,
+    autoHeight: false,
+    autoplayTimeout: 5500,
+  });
+});
